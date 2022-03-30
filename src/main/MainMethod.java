258f7cf6ab2e12c9805e4c609c91647a4894ea2f
stefanomom <54564403+stefanomom@users.noreply.github.com>
2022-03-30 12:42:59 -0500

    Update MainMethod.java

--- src/main/MainMethod.java
+++ src/main/MainMethod.java
@@ -8,10 +8,10 @@ public class MainMethod {
   public static void main(String args[]) {
     System.out.println("Choose from the following menu items:");
     System.out.println("1. tacos");
-    System.out.println("2. burrito");
-    System.out.println("3. quesadilla");
+    System.out.println("2. sushi");
+    System.out.println("3. pizza");
 
-Scanner scanchoice = new Scanner(System.in);
+Scanner mealChoice = new Scanner(System.in);
 System.out.println();
 
 int choice = 0;
@@ -19,21 +19,25 @@ int choice = 0;
     while(choice < 1 || choice > 3){
 
             System.out.println("Select \"1\", \"2\", \"3\"");
-            if(scanchoice.hasNextInt())
-            choice = scanchoice.nextInt();
+            if(mealChoice.hasNextInt())
+            choice = mealChoice.nextInt();
 
     }
 
+
+
      switch(choice){
         case 1:
-           System.out.println("tacos, $3.55, 156 calories. Would you like to place order?");
-
+           System.out.println("tacos, $3.55, 156 calories. 1 unit added to cart.");
+           System.out.println();
            break;
         case 2:
-           System.out.println("burrito, $4.20, 206 calories. Would you like to place order?");
+           System.out.println("sushi, $9.99, 200 calories. 1 unit added to cart.");
+           System.out.println();
            break;
         case 3:
-           System.out.println("quesadilla, $2.95, 170 calories. Would you like to place order?");
+           System.out.println("pizza, $2.00, 389 calories. 1 unit added to cart.");
+           System.out.println();
            break;
    }
   }
